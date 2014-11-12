e6e4f3adeb3d987d375c2755b47a0df7ecbc5d24
Vincent DURMONT <vdurmont@gmail.com>
2014-11-12 08:57:01 -0800

    When a credit card number is entered by the user, the focus is now requested to the next input if the maximum length has been reached (+ setup Roboletric)

--- creditcard4j-android/src/main/java/com/creditcard4j/CreditCardEditTextWatcher.java
+++ creditcard4j-android/src/main/java/com/creditcard4j/CreditCardEditTextWatcher.java
@@ -2,6 +2,7 @@ package com.creditcard4j;
 
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.widget.EditText;
 import com.creditcard4j.tools.CardNumberCleaner;
 
 /**
@@ -12,21 +13,44 @@ import com.creditcard4j.tools.CardNumberCleaner;
  */
 public class CreditCardEditTextWatcher implements TextWatcher {
     private final char spacingCharacter;
+    private final EditText nextInput;
 
     /**
      * Builds a CreditCardEditTextWatcher with the default spacing character (' ').
      */
     public CreditCardEditTextWatcher() {
-        this(' ');
+        this(null, ' ');
     }
 
     /**
-     * Builds a CreditCardEditTextWatcher with a chose spacing character.
+     * Builds a CreditCardEditTextWatcher with the next input.
+     * The next input will get focus when the card number is complete.
+     *
+     * @param nextInput next input
+     */
+    public CreditCardEditTextWatcher(EditText nextInput) {
+        this(nextInput, ' ');
+    }
+
+    /**
+     * Builds a CreditCardEditTextWatcher with a spacing character.
      *
      * @param spacingCharacter the spacing character to insert each 4 digits
      */
     public CreditCardEditTextWatcher(char spacingCharacter) {
+        this(null, spacingCharacter);
+    }
+
+    /**
+     * Builds a CreditCardEditTextWatcher with a spacing character.
+     * The next input will get focus when the card number is complete.
+     *
+     * @param nextInput        next input
+     * @param spacingCharacter the spacing character to insert each 4 digits
+     */
+    public CreditCardEditTextWatcher(EditText nextInput, char spacingCharacter) {
         this.spacingCharacter = spacingCharacter;
+        this.nextInput = nextInput;
     }
 
     @Override public void beforeTextChanged(CharSequence sequence, int start, int count, int after) {
@@ -40,7 +64,11 @@ public class CreditCardEditTextWatcher implements TextWatcher {
         String number = editable.toString();
         if (number.length() > 0) {
             String digitsOnly = CardNumberCleaner.clean(number);
-            if (digitsOnly.length() % 4 == 0) {
+            int length = digitsOnly.length();
+            CardBrand brand = CreditCard4J.getBrand(digitsOnly);
+            if (length == brand.getMaxLength() && this.nextInput != null) {
+                this.nextInput.requestFocus();
+            } else if (length % 4 == 0) {
                 editable.append(this.spacingCharacter);
             }
         }
--- creditcard4j-android/src/test/java/com/creditcard4j/CreditCardEditTextWatcherTest.java
+++ creditcard4j-android/src/test/java/com/creditcard4j/CreditCardEditTextWatcherTest.java
@@ -1,16 +1,27 @@
 package com.creditcard4j;
 
 import android.text.Editable;
+import android.text.SpannableStringBuilder;
 import android.text.TextWatcher;
+import android.widget.EditText;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.junit.runners.JUnit4;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.fail;
+import static org.junit.Assert.assertTrue;
 
-@RunWith(JUnit4.class)
+@RunWith(RobolectricTestRunner.class)
+@Config(manifest = Config.NONE)
 public class CreditCardEditTextWatcherTest {
+    @Before
+    public void setUp() {
+        CreditCard4J.resetConfig();
+    }
+
     @Test
     public void enter_a_digit_returns_the_text() {
         // GIVEN
@@ -56,8 +67,34 @@ public class CreditCardEditTextWatcherTest {
     }
 
     @Test
-    public void test_all_the_world() {
-        fail("Missing tests.");
+    public void enter_16_digits_with_a_specified_spacing_character_returns_the_text_with_the_spacing_character_each_4_digits() {
+        // GIVEN
+        CreditCard4J.ALLOWED_CARD_NUMBERS_SPACING_CHARACTERS = new String[]{"&"};
+        CreditCardEditTextWatcher cc = new CreditCardEditTextWatcher('&');
+
+        // WHEN
+        String result = "";
+        for (int i = 1; i <= 16; i++) {
+            result = simulateTextInput(cc, result, String.valueOf(i % 10));
+        }
+
+        // THEN
+        assertEquals("1234&5678&9012&3456&", result);
+    }
+
+    @Test
+    public void enter_the_maxLength_digits_for_the_card_brand_focuses_on_the_next_input() {
+        // GIVEN
+        EditText editText = new EditText(Robolectric.application);
+        CreditCardEditTextWatcher cc = new CreditCardEditTextWatcher(editText);
+        String number = "4242424242424242"; // VISA
+        int maxLenght = CreditCard4J.getBrand(number).getMaxLength();
+
+        // WHEN
+        simulateTextInput(cc, number.substring(0, maxLenght - 1), String.valueOf(number.charAt(maxLenght - 1)));
+
+        // THEN
+        assertTrue(editText.hasFocus());
     }
 
     private static String simulateTextInput(TextWatcher tw, String original, String input) {
@@ -67,7 +104,8 @@ public class CreditCardEditTextWatcherTest {
         String newText = original + input;
         tw.onTextChanged(newText, 1, newText.length() - 1, 1);
 
-        Editable editable = new MyEditable(newText);
+
+        Editable editable = new SpannableStringBuilder(newText);
         tw.afterTextChanged(editable);
         return editable.toString();
     }

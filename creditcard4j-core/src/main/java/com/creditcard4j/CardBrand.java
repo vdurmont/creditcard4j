94c54de1d299b8c3310e143530b51631ba170674
Vincent DURMONT <vdurmont@gmail.com>
2014-11-11 21:17:02 -0800

    Adding the expected maximum length for each card brand

--- creditcard4j-core/src/main/java/com/creditcard4j/CardBrand.java
+++ creditcard4j-core/src/main/java/com/creditcard4j/CardBrand.java
@@ -3,20 +3,24 @@ package com.creditcard4j;
 /**
  * Enumeration describing the various credit card brands.
  *
+ * Information retrieved on https://en.wikipedia.org/wiki/Bank_card_number
+ *
  * @author Vincent DURMONT [vdurmont@gmail.com]
  */
 public enum CardBrand {
-    AMERICAN_EXPRESS("34", "37"),
-    DINERS_CLUB("300", "301", "302", "303", "304", "305", "36", "38"),
-    DISCOVER("6011", "65"),
-    JCB("2131", "1800", "35"),
-    MASTERCARD("51", "52", "53", "54", "55"),
-    UNKNOWN,
-    VISA("4");
+    AMERICAN_EXPRESS(15, "34", "37"),
+    DINERS_CLUB(16, "300", "301", "302", "303", "304", "305", "36", "38"),
+    DISCOVER(16, "6011", "65"),
+    JCB(16, "2131", "1800", "35"),
+    MASTERCARD(16, "51", "52", "53", "54", "55"),
+    UNKNOWN(19),
+    VISA(16, "4");
 
+    private final int maxLength;
     private final String[] prefixes;
 
-    private CardBrand(String... prefixes) {
+    private CardBrand(int maxLength, String... prefixes) {
+        this.maxLength = maxLength;
         this.prefixes = prefixes;
     }
 
@@ -28,5 +32,14 @@ public enum CardBrand {
      */
     public String[] getPrefixes() {
         return this.prefixes;
+    }
+
+    /**
+     * Returns the maximum length of the card number according to the brand.
+     *
+     * @return the maximum length
+     */
+    public int getMaxLength() {
+        return this.maxLength;
     }
 }
